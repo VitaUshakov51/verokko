3b725d727dba1ccc63a6f1091c131ce9e27cf5bf
VitaUshakov51 <vitavita-98@yandex.ru>
2024-03-08 18:10:49 +0300

    complete hero

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,10 +3,12 @@
     const burger = document.getElementById('burger');
     const menu = document.getElementById('menu');
     const shadow = document.getElementById('shadow')
+    const body = document.getElementById('body')
     burger.addEventListener('click', () => {
         burger.classList.toggle('active');
         menu.classList.toggle('active')
         shadow.classList.toggle('show')
+        body.classList.toggle('noScroll')
     })
 
 })()
